f209c0b12694ec0c6166ddfa54af3633db9663f4
Babushkin, Andrey <andrey.babushkin@firma.seznam.cz>
2023-05-02 18:28:37 +0200

    misc: requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ setup(
     version="0.1",
     packages=["gmphd_fusion"],
     test_suite="tests",
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "scipy",
+        "seaborn",
+        "stonesoup",
+    ],
     url="https://github.com/mannannlegur/gmphd-fusion",
     license_files=("LICENSE",),
     author="Andrey Babushkin",
